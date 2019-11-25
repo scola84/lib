cb481e26cf2b18a77d76a85f8fd3c5df1924dd08
M.C. van den Boogaart <scola@kvdb.name>
2019-11-25 16:17:10 +0100

    Allow duration formatting

--- src/worker/core/builder/helper/formatter/luxon.js
+++ src/worker/core/builder/helper/formatter/luxon.js
@@ -6,14 +6,20 @@ export function l (value, options = '', locale = 'nl_NL') {
     : options.split(';')
 
   const [
-    format = 'D'
+    format = 'D',
+    type = 'dat'
   ] = foptions
 
   const loptions = {
     locale: locale.replace('_', '-')
   }
 
-  return DateTime
-    .fromISO(value)
-    .toFormat(format, loptions)
+  let date = DateTime
+    .fromJSDate(new Date(value), loptions)
+
+  if (type === 'dur') {
+    date = date.diffNow()
+  }
+
+  return date.toFormat(format)
 }

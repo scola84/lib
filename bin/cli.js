ccf17a7a10e71f6633e0a7cda5e03db464932ea0
M.C. van den Boogaart <scola@kvdb.name>
2021-04-02 11:55:03 +0200

    feat(cli): implement as commander

--- bin/cli.js
+++ bin/cli.js
@@ -1,112 +1,81 @@
 #!/usr/bin/env node
 /* eslint-disable no-console */
 
+const commander = require('commander')
 const fs = require('fs')
-const path = require('path')
 const sqlts = require('@rmp135/sql-ts')
 
-const command = process.argv[2]
+commander
+  .command('sql-ts <source-db> <target-path>')
+  .description('Transforms SQL tables into TS entities')
+  .option('-d, --driver <driver>', 'the database driver, see http://knexjs.org/#Installation-node', 'mysql2')
+  .action((source, target, options) => {
+    const typeMap = {
+      Buffer: ['binary', 'varbinary', 'image', 'UDT'],
+      Date: ['datetime', 'timestamp', 'date', 'time', 'timestamp', 'datetime2', 'smalldatetime', 'datetimeoffset'],
+      Object: ['json', 'TVP'],
+      boolean: ['bit', 'boolean', 'bool'],
+      number: ['tinyint', 'int', 'numeric', 'integer', 'real', 'smallint', 'decimal', 'float', 'float4', 'float8', 'double precision', 'double', 'dec', 'fixed', 'year', 'serial', 'bigserial', 'int4', 'money', 'smallmoney'],
+      string: ['nchar', 'nvarchar', 'varchar', 'char', 'tinytext', 'text', 'longtext', 'mediumtext', 'ntext', 'varbinary', 'uuid', 'uniqueidentifier', 'character varying', 'bigint', 'xml']
+    }
 
-if (command === 'sql-ts') {
-  const options = process.argv
-    .slice(3)
-    .reduce((result, value, index) => {
-      return value.startsWith('-') ? {
-        ...result,
-        [value.slice(2)]: process.argv[index + 4]
-      } : result
-    }, {
-      driver: '',
-      dsn: '',
-      path: ''
-    })
+    sqlts
+      .toObject({
+        client: options.driver,
+        connection: source,
+        // eslint-disable-next-line no-template-curly-in-string
+        interfaceNameFormat: '${table}',
+        tableNameCasing: 'pascal',
+        typeMap: {
+          Date: [...typeMap.Date, 'timestamptz'],
+          number: [...typeMap.number, 'int8'],
+          unknown: ['json']
+        }
+      })
+      .then((database) => {
+        fs.mkdirSync(target, { recursive: true })
+        return database
+      })
+      .then((database) => {
+        for (const table of database.tables) {
+          table.columns.sort((a, b) => {
+            return a.propertyName > b.propertyName ? 1 : -1
+          })
+        }
 
-  if (options.driver === '') {
-    console.log('scola: provide a driver with --driver (see http://knexjs.org/#Installation-node)')
-    process.exit()
-  }
+        return database
+      })
+      .then((database) => {
+        for (const table of database.tables) {
+          fs.writeFileSync(
+            `${target}/${table.name.replace('_', '-')}.ts`,
+            sqlts
+              .fromObject({
+                enums: [],
+                tables: [table]
+              }, {})
+              .replace(/\/\*[\s\S]*\*\/\n*/gu, '')
+              .replace(/"/gu, '')
+              .replace(/&lt;/gu, '<')
+              .replace(/&gt;/gu, '>')
+              .replace(/\s\n/gu, '\n')
+          )
+        }
+        return database
+      })
+      .then((database) => {
+        const exports = database.tables
+          .map((table) => {
+            return `export * from './${table.name.replace('_', '-')}'`
+          })
+          .sort()
+          .join('\n')
 
-  if (options.dsn === '') {
-    console.log('scola: provide a source DSN with --dsn')
-    process.exit()
-  }
+        fs.writeFileSync(`${target}/index.ts`, `${exports}\n`)
+      })
+      .catch((error) => {
+        console.log(String(error))
+      })
+  })
 
-  if (options.path === '') {
-    console.log('scola: provide a target directory with --path')
-    process.exit()
-  }
-
-  options.path = `${path.resolve(options.path)}/`
-
-  const typeMap = {
-    Buffer: ['binary', 'varbinary', 'image', 'UDT'],
-    Date: ['datetime', 'timestamp', 'date', 'time', 'timestamp', 'datetime2', 'smalldatetime', 'datetimeoffset'],
-    Object: ['json', 'TVP'],
-    boolean: ['bit', 'boolean', 'bool'],
-    number: ['tinyint', 'int', 'numeric', 'integer', 'real', 'smallint', 'decimal', 'float', 'float4', 'float8', 'double precision', 'double', 'dec', 'fixed', 'year', 'serial', 'bigserial', 'int4', 'money', 'smallmoney'],
-    string: ['nchar', 'nvarchar', 'varchar', 'char', 'tinytext', 'text', 'longtext', 'mediumtext', 'ntext', 'varbinary', 'uuid', 'uniqueidentifier', 'character varying', 'bigint', 'xml']
-  }
-
-  sqlts
-    .toObject({
-      client: options.driver,
-      connection: options.dsn,
-      // eslint-disable-next-line no-template-curly-in-string
-      interfaceNameFormat: '${table}',
-      tableNameCasing: 'pascal',
-      typeMap: {
-        Date: [...typeMap.Date, 'timestamptz'],
-        number: [...typeMap.number, 'int8'],
-        unknown: ['json']
-      }
-    })
-    .then((database) => {
-      fs.mkdirSync(options.path, { recursive: true })
-      return database
-    })
-    .then((database) => {
-      for (const table of database.tables) {
-        table.columns.sort((a, b) => {
-          return a.propertyName > b.propertyName ? 1 : -1
-        })
-      }
-
-      return database
-    })
-    .then((database) => {
-      for (const table of database.tables) {
-        fs.writeFileSync(
-          `${options.path}${table.name.replace('_', '-')}.ts`,
-          sqlts
-            .fromObject({
-              enums: [],
-              tables: [table]
-            }, {})
-            .replace(/\/\*[\s\S]*\*\/\n*/gu, '')
-            .replace(/"/gu, '')
-            .replace(/&lt;/gu, '<')
-            .replace(/&gt;/gu, '>')
-            .replace(/\s\n/gu, '\n')
-        )
-      }
-      return database
-    })
-    .then((database) => {
-      const exports = database.tables
-        .map((table) => {
-          return `export * from './${table.name.replace('_', '-')}'`
-        })
-        .sort()
-        .join('\n')
-
-      fs.writeFileSync(
-        `${options.path}index.ts`,
-        `${exports}\n`
-      )
-    })
-    .catch((error) => {
-      console.error(error)
-    })
-} else {
-  console.log(`scola: '${command}' is not scola command.`)
-}
+commander.parse()

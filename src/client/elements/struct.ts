a491a1a17ff96dc179ba69a0646b8d7e34fd9722
M.C. van den Boogaart <scola@kvdb.name>
2021-08-28 13:34:44 +0200

    feat(struct): add set data event

--- src/client/elements/struct.ts
+++ src/client/elements/struct.ts
@@ -1,16 +1,38 @@
 import { NodeElement } from './node'
 import type { PropertyValues } from 'lit'
+import type { Struct } from '../../common'
 import { customElement } from 'lit/decorators.js'
 
 declare global {
   interface HTMLElementTagNameMap {
     'scola-struct': StructElement
   }
+
+  interface WindowEventMap {
+    'scola-struct-set-data': CustomEvent
+  }
 }
 
 @customElement('scola-struct')
 export class StructElement extends NodeElement {
+  protected handleSetDataBound: (event: CustomEvent) => void
+
   protected updaters = StructElement.updaters
+
+  public constructor () {
+    super()
+    this.handleSetDataBound = this.handleSetData.bind(this)
+  }
+
+  public connectedCallback (): void {
+    window.addEventListener('scola-struct-set-data', this.handleSetDataBound)
+    super.connectedCallback()
+  }
+
+  public disconnectedCallback (): void {
+    window.removeEventListener('scola-struct-set-data', this.handleSetDataBound)
+    super.disconnectedCallback()
+  }
 
   public update (properties: PropertyValues): void {
     if (properties.has('data')) {
@@ -26,5 +48,9 @@ export class StructElement extends NodeElement {
     } else {
       this.setLeafData()
     }
+  }
+
+  protected handleSetData (event: CustomEvent<Struct | null>): void {
+    this.data = event.detail?.data
   }
 }
